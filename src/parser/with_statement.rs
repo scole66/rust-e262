8a9c9eb98e375618bd565ae8f1faed2b005644c0
Steven Cole <scole66@gmail.com>
2021-01-29 13:07:37 -0800

    Add with statement

--- src/parser/with_statement.rs
+++ src/parser/with_statement.rs
@@ -2,16 +2,23 @@ use std::fmt;
 use std::io::Result as IoResult;
 use std::io::Write;
 
+use super::comma_operator::Expression;
 use super::scanner::Scanner;
+use super::statements_and_declarations::Statement;
 use super::*;
-use crate::prettyprint::{prettypad, PrettyPrint, Spot};
+use crate::prettyprint::{pprint_token, prettypad, PrettyPrint, Spot};
 
+// WithStatement[Yield, Await, Return] :
+//      with ( Expression[+In, ?Yield, ?Await] ) Statement[?Yield, ?Await, ?Return]
 #[derive(Debug)]
-pub enum WithStatement {}
+pub struct WithStatement {
+    expression: Box<Expression>,
+    statement: Box<Statement>,
+}
 
 impl fmt::Display for WithStatement {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "unimplemented")
+        write!(f, "with ( {} ) {}", self.expression, self.statement)
     }
 }
 
@@ -20,26 +27,61 @@ impl PrettyPrint for WithStatement {
     where
         T: Write,
     {
-        let (first, _successive) = prettypad(pad, state);
-        writeln!(writer, "{}WithStatement: {}", first, self)
+        let (first, successive) = prettypad(pad, state);
+        writeln!(writer, "{}WithStatement: {}", first, self)?;
+        self.expression
+            .pprint_with_leftpad(writer, &successive, Spot::NotFinal)?;
+        self.statement.pprint_with_leftpad(writer, &successive, Spot::Final)
     }
 
     fn concise_with_leftpad<T>(&self, writer: &mut T, pad: &str, state: Spot) -> IoResult<()>
     where
         T: Write,
     {
-        todo!()
+        let (first, successive) = prettypad(pad, state);
+        writeln!(writer, "{}WithStatement: {}", first, self)?;
+        pprint_token(writer, "with", &successive, Spot::NotFinal)?;
+        pprint_token(writer, "(", &successive, Spot::NotFinal)?;
+        self.expression
+            .concise_with_leftpad(writer, &successive, Spot::NotFinal)?;
+        pprint_token(writer, ")", &successive, Spot::NotFinal)?;
+        self.statement.concise_with_leftpad(writer, &successive, Spot::Final)
     }
 }
 
 impl WithStatement {
     pub fn parse(
-        _parser: &mut Parser,
-        _scanner: Scanner,
-        _yield_flag: bool,
-        _await_flag: bool,
-        _return_flag: bool,
+        parser: &mut Parser,
+        scanner: Scanner,
+        yield_flag: bool,
+        await_flag: bool,
+        return_flag: bool,
     ) -> Result<Option<(Box<Self>, Scanner)>, String> {
+        let (with_tok, after_with) =
+            scanner::scan_token(&scanner, parser.source, scanner::ScanGoal::InputElementRegExp);
+        if matches!(with_tok, scanner::Token::Identifier(id) if id.keyword_id == Some(scanner::Keyword::With)) {
+            let (open, after_open) =
+                scanner::scan_token(&after_with, parser.source, scanner::ScanGoal::InputElementDiv);
+            if open == scanner::Token::LeftParen {
+                let pot_exp = Expression::parse(parser, after_open, true, yield_flag, await_flag)?;
+                if let Some((exp, after_exp)) = pot_exp {
+                    let (close, after_close) =
+                        scanner::scan_token(&after_exp, parser.source, scanner::ScanGoal::InputElementDiv);
+                    if close == scanner::Token::RightParen {
+                        let pot_stmt = Statement::parse(parser, after_close, yield_flag, await_flag, return_flag)?;
+                        if let Some((stmt, after_stmt)) = pot_stmt {
+                            return Ok(Some((
+                                Box::new(WithStatement {
+                                    expression: exp,
+                                    statement: stmt,
+                                }),
+                                after_stmt,
+                            )));
+                        }
+                    }
+                }
+            }
+        }
         Ok(None)
     }
 }
