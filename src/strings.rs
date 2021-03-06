b49bec1b0dfeefb58dad60b67ae1127eedab05ee
Steven Cole <scole66@gmail.com>
2021-03-06 14:33:23 -0800

    Added String Interning

--- src/strings.rs
+++ src/strings.rs
@@ -1,63 +1,170 @@
+use lasso::{Rodeo, Spur};
+use std::cell::RefCell;
+use std::char::{decode_utf16, DecodeUtf16};
 use std::fmt;
-use std::rc::Rc;
+use std::iter::Cloned;
+use std::slice::Iter;
+use std::str::EncodeUtf16;
+
+// So: JS String.
+//
+// At it's core, this is simply a Vec<u16>, but interned for speed & storage.
+//
+// The API for pulling out pieces of them hasn't really been defined yet.
+
+thread_local! {
+    static STRING_POOL: RefCell<Rodeo<Spur>> = Default::default();
+}
 
 #[derive(PartialEq, Clone)]
 pub struct JSString {
-    string: Rc<Vec<u16>>,
+    s: Spur,
 }
 
-impl fmt::Debug for JSString {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{:?}", String::from_utf16_lossy(&self.string)))
+struct CharToU16Iterator<'a> {
+    source: EncodeUtf16<'a>,
+}
+
+impl<'a> CharToU16Iterator<'a> {
+    fn new<T>(source: T) -> Self
+    where
+        T: Into<&'a str>,
+    {
+        CharToU16Iterator { source: source.into().encode_utf16() }
     }
 }
 
-impl fmt::Display for JSString {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{}", String::from_utf16_lossy(&self.string)))
-    }
-}
+impl<'a> Iterator for CharToU16Iterator<'a> {
+    type Item = u16;
 
-impl std::cmp::PartialEq<&str> for JSString {
-    fn eq(&self, other: &&str) -> bool {
-        let mut iter_vec = self.string.iter();
-        let mut iter_chars = (*other).chars();
-        loop {
-            let left = iter_vec.next();
-            let right = iter_chars.next();
-            if left.is_none() && right.is_none() {
-                return true;
+    fn next(&mut self) -> Option<Self::Item> {
+        let ch = self.source.next()?;
+        match ch {
+            0xE00E | 0xE00D => {
+                let ch2 = self.source.next();
+                match ch2 {
+                    None => Some(ch),
+                    Some(c) => Some(((ch & 0xF) << 12) | c),
+                }
             }
-            if left.is_none() || right.is_none() {
-                return false;
-            }
-            if *(left.unwrap()) as u32 != right.unwrap() as u32 {
-                return false;
-            }
+            c => Some(c),
         }
     }
 }
 
-impl From<&str> for JSString {
-    fn from(source: &str) -> Self {
-        let mut result = Vec::with_capacity(source.len());
-        for val in source.encode_utf16() {
-            result.push(val);
+struct U16ToCharIterator<'a> {
+    source: DecodeUtf16<Cloned<Iter<'a, u16>>>,
+    next_result: Option<char>,
+}
+
+impl<'a> U16ToCharIterator<'a> {
+    fn new(source: Cloned<Iter<'a, u16>>) -> Self {
+        let new_iter = decode_utf16(source);
+        U16ToCharIterator { source: new_iter, next_result: None }
+    }
+}
+
+impl<'a> Iterator for U16ToCharIterator<'a> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(ch) = self.next_result {
+            self.next_result = None;
+            return Some(ch);
         }
-        JSString { string: Rc::new(result) }
+        let ch = self.source.next()?;
+        match ch {
+            Err(e) => {
+                let unpaired = e.unpaired_surrogate() as u32;
+                self.next_result = Some(std::char::from_u32(unpaired & 0xFFF).unwrap());
+                Some('\u{E00D}')
+            }
+            Ok('\u{E00D}') => {
+                self.next_result = Some('\u{000D}');
+                Some('\u{E00E}')
+            }
+            Ok('\u{E00E}') => {
+                self.next_result = Some('\u{000E}');
+                Some('\u{E00E}')
+            }
+            Ok(c) => Some(c),
+        }
     }
 }
 
 impl From<&[u16]> for JSString {
     fn from(source: &[u16]) -> Self {
-        let mut result = Vec::with_capacity(source.len());
-        result.extend_from_slice(source);
-        JSString { string: Rc::new(result) }
+        let iter = U16ToCharIterator::new(source.iter().cloned());
+        let encoded: String = iter.collect();
+        Self::from(encoded)
     }
 }
 
 impl From<Vec<u16>> for JSString {
     fn from(source: Vec<u16>) -> Self {
-        JSString { string: Rc::new(source) }
+        Self::from(source.as_slice())
+    }
+}
+
+impl From<&str> for JSString {
+    fn from(source: &str) -> Self {
+        STRING_POOL.with(|sp| {
+            let mut pool = sp.borrow_mut();
+            let key = pool.get_or_intern(source);
+            JSString { s: key }
+        })
+    }
+}
+
+impl From<String> for JSString {
+    fn from(source: String) -> Self {
+        Self::from(source.as_str())
+    }
+}
+
+impl fmt::Debug for JSString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        STRING_POOL.with(|sp| {
+            let pool = sp.borrow();
+            let s = pool.resolve(&self.s);
+            f.write_fmt(format_args!("{:?}", s))
+        })
+    }
+}
+
+impl fmt::Display for JSString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        STRING_POOL.with(|sp| {
+            let pool = sp.borrow();
+            let s = pool.resolve(&self.s);
+            f.write_fmt(format_args!("{}", s))
+        })
+    }
+}
+
+impl std::cmp::PartialEq<&str> for JSString {
+    fn eq(&self, other: &&str) -> bool {
+        STRING_POOL.with(|sp| {
+            let pool = sp.borrow();
+            let maybe_other_key = pool.get(*other);
+            if let Some(other_key) = maybe_other_key {
+                self.s == other_key
+            } else {
+                false
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_u16_test() {
+        let src: &[u16] = &[66, 111, 98]; // Bob
+        let res = JSString::from(src);
+        let display = format!("{}", res);
+        assert_eq!(display, "Bob");
     }
 }
