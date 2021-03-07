64ac51bf45370f2e2db66423dcfbcf23285c7212
Steven Cole <scole66@gmail.com>
2021-03-07 14:57:56 -0800

    Add tests for BreakStatement parsing

--- src/parser/break_statement.rs
+++ src/parser/break_statement.rs
@@ -54,21 +54,73 @@ impl PrettyPrint for BreakStatement {
 
 impl BreakStatement {
     pub fn parse(parser: &mut Parser, scanner: Scanner, yield_flag: bool, await_flag: bool) -> Result<(Box<Self>, Scanner), ParseError> {
-        scan_for_keyword(scanner, parser.source, ScanGoal::InputElementRegExp, Keyword::Break).and_then(|after_break| {
-            no_line_terminator(after_break, parser.source)
-                .and_then(|()| {
-                    LabelIdentifier::parse(parser, after_break, yield_flag, await_flag).and_then(|(li, after_li)| {
-                        scan_for_punct(after_li, parser.source, ScanGoal::InputElementDiv, Punctuator::Semicolon).map(|after_semi| (Box::new(BreakStatement::Labelled(li)), after_semi))
-                    })
-                })
-                .otherwise(|| scan_for_punct(after_break, parser.source, ScanGoal::InputElementDiv, Punctuator::Semicolon).map(|after_semi| (Box::new(BreakStatement::Bare), after_semi)))
+        let after_break = scan_for_keyword(scanner, parser.source, ScanGoal::InputElementRegExp, Keyword::Break)?;
+        scan_for_punct(after_break, parser.source, ScanGoal::InputElementDiv, Punctuator::Semicolon).map(|after_semi| (Box::new(BreakStatement::Bare), after_semi)).otherwise(|| {
+            no_line_terminator(after_break, parser.source)?;
+            let (li, after_li) = LabelIdentifier::parse(parser, after_break, yield_flag, await_flag)?;
+            let after_semi = scan_for_punct(after_li, parser.source, ScanGoal::InputElementDiv, Punctuator::Semicolon)?;
+            Ok((Box::new(BreakStatement::Labelled(li)), after_semi))
         })
     }
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::testhelp::{check, check_none, chk_scan, newparser};
-//    use super::*;
-//    use crate::prettyprint::testhelp::{pretty_check, pretty_error_validate};
-//}
+#[cfg(test)]
+mod tests {
+    use super::testhelp::{check, check_err, chk_scan, newparser};
+    use super::*;
+    use crate::prettyprint::testhelp::{concise_check, concise_error_validate, pretty_check, pretty_error_validate};
+
+    // BREAK STATEMENT
+    #[test]
+    fn break_statement_test_01() {
+        let (node, scanner) = check(BreakStatement::parse(&mut newparser("break;"), Scanner::new(), false, false));
+        chk_scan(&scanner, 6);
+        pretty_check(&*node, "BreakStatement: break ;", vec![]);
+        concise_check(&*node, "BreakStatement: break ;", vec!["Keyword: break", "Punctuator: ;"]);
+        format!("{:?}", node);
+    }
+    #[test]
+    fn break_statement_test_02() {
+        let (node, scanner) = check(BreakStatement::parse(&mut newparser("break a;"), Scanner::new(), false, false));
+        chk_scan(&scanner, 8);
+        pretty_check(&*node, "BreakStatement: break a ;", vec!["LabelIdentifier: a"]);
+        concise_check(&*node, "BreakStatement: break a ;", vec!["Keyword: break", "IdentifierName: a", "Punctuator: ;"]);
+        format!("{:?}", node);
+    }
+    #[test]
+    fn break_statement_test_err_01() {
+        check_err(BreakStatement::parse(&mut newparser(""), Scanner::new(), false, false), "‘break’ expected", 1, 1);
+    }
+    #[test]
+    fn break_statement_test_err_02() {
+        check_err(BreakStatement::parse(&mut newparser("break\n"), Scanner::new(), false, false), "‘;’ expected", 1, 6);
+    }
+    #[test]
+    fn break_statement_test_err_03() {
+        check_err(BreakStatement::parse(&mut newparser("break a"), Scanner::new(), false, false), "‘;’ expected", 1, 8);
+    }
+    #[test]
+    fn break_statement_test_err_04() {
+        check_err(BreakStatement::parse(&mut newparser("break for"), Scanner::new(), false, false), "‘;’ expected", 1, 6);
+    }
+    #[test]
+    fn break_statement_test_prettyerrors_1() {
+        let (item, _) = BreakStatement::parse(&mut newparser("break;"), Scanner::new(), false, false).unwrap();
+        pretty_error_validate(*item);
+    }
+    #[test]
+    fn break_statement_test_prettyerrors_2() {
+        let (item, _) = BreakStatement::parse(&mut newparser("break label;"), Scanner::new(), false, false).unwrap();
+        pretty_error_validate(*item);
+    }
+    #[test]
+    fn break_statement_test_conciseerrors_1() {
+        let (item, _) = BreakStatement::parse(&mut newparser("break;"), Scanner::new(), false, false).unwrap();
+        concise_error_validate(*item);
+    }
+    #[test]
+    fn break_statement_test_conciseerrors_2() {
+        let (item, _) = BreakStatement::parse(&mut newparser("break label;"), Scanner::new(), false, false).unwrap();
+        concise_error_validate(*item);
+    }
+}
