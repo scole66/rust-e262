f2bd2daae59777c7f0fb2962c6a8273b91e21b7a
Steven Cole <scole66@gmail.com>
2021-03-09 20:43:58 -0800

    Tests for ExressionStatement

--- src/parser/expression_statement.rs
+++ src/parser/expression_statement.rs
@@ -75,9 +75,59 @@ impl ExpressionStatement {
     }
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::testhelp::{check, check_none, chk_scan, newparser};
-//    use super::*;
-//    use crate::prettyprint::testhelp::{pretty_check, pretty_error_validate};
-//}
+#[cfg(test)]
+mod tests {
+    use super::testhelp::{check, check_err, chk_scan, newparser};
+    use super::*;
+    use crate::prettyprint::testhelp::{concise_check, concise_error_validate, pretty_check, pretty_error_validate};
+
+    // EXPRESSION STATEMENT
+    #[test]
+    fn expression_statement_test_01() {
+        let (node, scanner) = check(ExpressionStatement::parse(&mut newparser("a;"), Scanner::new(), false, false));
+        chk_scan(&scanner, 2);
+        pretty_check(&*node, "ExpressionStatement: a ;", vec!["Expression: a"]);
+        concise_check(&*node, "ExpressionStatement: a ;", vec!["IdentifierName: a", "Punctuator: ;"]);
+        format!("{:?}", node);
+        pretty_error_validate(&*node);
+        concise_error_validate(&*node);
+    }
+    #[test]
+    fn expression_statement_test_02() {
+        let (node, scanner) = check(ExpressionStatement::parse(&mut newparser("async;"), Scanner::new(), false, false));
+        chk_scan(&scanner, 6);
+        pretty_check(&*node, "ExpressionStatement: async ;", vec!["Expression: async"]);
+        concise_check(&*node, "ExpressionStatement: async ;", vec!["IdentifierName: async", "Punctuator: ;"]);
+        format!("{:?}", node);
+        pretty_error_validate(&*node);
+        concise_error_validate(&*node);
+    }
+    #[test]
+    fn expression_statement_test_err_01() {
+        check_err(ExpressionStatement::parse(&mut newparser(""), Scanner::new(), false, false), "Expression expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_02() {
+        check_err(ExpressionStatement::parse(&mut newparser("{"), Scanner::new(), false, false), "ExpressionStatement expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_03() {
+        check_err(ExpressionStatement::parse(&mut newparser("function"), Scanner::new(), false, false), "ExpressionStatement expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_04() {
+        check_err(ExpressionStatement::parse(&mut newparser("class"), Scanner::new(), false, false), "ExpressionStatement expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_05() {
+        check_err(ExpressionStatement::parse(&mut newparser("let ["), Scanner::new(), false, false), "ExpressionStatement expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_06() {
+        check_err(ExpressionStatement::parse(&mut newparser("async function"), Scanner::new(), false, false), "ExpressionStatement expected", 1, 1);
+    }
+    #[test]
+    fn expression_statement_test_err_07() {
+        check_err(ExpressionStatement::parse(&mut newparser("0"), Scanner::new(), false, false), "‘;’ expected", 1, 2);
+    }
+}
