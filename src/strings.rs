cfc461e6f57a3b6af22677f5d7333011336902b0
Steven Cole <scole66@gmail.com>
2021-03-20 10:04:51 -0700

    Remove string interning
    I tried switching to a [u16] interner instead (which worked fine), but I
    was having lifetime problems dealing with the underlying buffers when it
    came time to _use_ them. The impact is that I'd have to _copy_ out of
    the pool every time we wanted to use a string, and if that's the case,
    what's the point?
    
    I'll come back to this, maybe, in the future. It doesn't really impact
    this as a toy project, after all.

--- src/strings.rs
+++ src/strings.rs
@@ -1,10 +1,5 @@
-use lasso::{Rodeo, Spur};
-use std::cell::RefCell;
-use std::char::{decode_utf16, DecodeUtf16};
 use std::fmt;
-use std::iter::Cloned;
-use std::slice::Iter;
-use std::str::EncodeUtf16;
+use std::ops::Index;
 
 // So: JS String.
 //
@@ -12,113 +7,38 @@ use std::str::EncodeUtf16;
 //
 // The API for pulling out pieces of them hasn't really been defined yet.
 
-thread_local! {
-    static STRING_POOL: RefCell<Rodeo<Spur>> = Default::default();
-}
-
 #[derive(PartialEq, Clone)]
 pub struct JSString {
-    s: Spur,
+    s: Vec<u16>,
 }
 
 impl JSString {
-    fn words<'a>(&self, pool: &'a Rodeo<Spur>) -> CharToU16Iterator<'a> {
-        CharToU16Iterator::new(pool.resolve(&self.s))
+    pub fn as_slice(&self) -> &[u16] {
+        self.s.as_slice()
     }
-}
 
-struct CharToU16Iterator<'a> {
-    source: EncodeUtf16<'a>,
-}
-
-impl<'a> CharToU16Iterator<'a> {
-    fn new<T>(source: T) -> Self
-    where
-        T: Into<&'a str>,
-    {
-        CharToU16Iterator { source: source.into().encode_utf16() }
-    }
-}
-
-impl<'a> Iterator for CharToU16Iterator<'a> {
-    type Item = u16;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let ch = self.source.next()?;
-        match ch {
-            0xE00E | 0xE00D => {
-                let ch2 = self.source.next();
-                match ch2 {
-                    None => Some(ch),
-                    Some(c) => Some(((ch & 0xF) << 12) | c),
-                }
-            }
-            c => Some(c),
-        }
-    }
-}
-
-struct U16ToCharIterator<'a> {
-    source: DecodeUtf16<Cloned<Iter<'a, u16>>>,
-    next_result: Option<char>,
-}
-
-impl<'a> U16ToCharIterator<'a> {
-    fn new(source: Cloned<Iter<'a, u16>>) -> Self {
-        let new_iter = decode_utf16(source);
-        U16ToCharIterator { source: new_iter, next_result: None }
-    }
-}
-
-impl<'a> Iterator for U16ToCharIterator<'a> {
-    type Item = char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(ch) = self.next_result {
-            self.next_result = None;
-            return Some(ch);
-        }
-        let ch = self.source.next()?;
-        match ch {
-            Err(e) => {
-                let unpaired = e.unpaired_surrogate() as u32;
-                self.next_result = Some(std::char::from_u32(unpaired & 0xFFF).unwrap());
-                Some('\u{E00D}')
-            }
-            Ok('\u{E00D}') => {
-                self.next_result = Some('\u{000D}');
-                Some('\u{E00E}')
-            }
-            Ok('\u{E00E}') => {
-                self.next_result = Some('\u{000E}');
-                Some('\u{E00E}')
-            }
-            Ok(c) => Some(c),
-        }
-    }
-}
-
-impl From<&[u16]> for JSString {
-    fn from(source: &[u16]) -> Self {
-        let iter = U16ToCharIterator::new(source.iter().cloned());
-        let encoded: String = iter.collect();
-        Self::from(encoded)
+    pub fn len(&self) -> usize {
+        self.s.len()
     }
 }
 
 impl From<Vec<u16>> for JSString {
     fn from(source: Vec<u16>) -> Self {
-        Self::from(source.as_slice())
+        Self { s: source }
+    }
+}
+
+impl From<&[u16]> for JSString {
+    fn from(source: &[u16]) -> Self {
+        let mut v: Vec<u16> = Vec::with_capacity(source.len());
+        v.extend_from_slice(source);
+        Self::from(v)
     }
 }
 
 impl From<&str> for JSString {
     fn from(source: &str) -> Self {
-        STRING_POOL.with(|sp| {
-            let mut pool = sp.borrow_mut();
-            let key = pool.get_or_intern(source);
-            JSString { s: key }
-        })
+        Self::from(source.encode_utf16().collect::<Vec<u16>>())
     }
 }
 
@@ -128,38 +48,132 @@ impl From<String> for JSString {
     }
 }
 
+
 impl fmt::Debug for JSString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let s = pool.resolve(&self.s);
-            f.write_fmt(format_args!("{:?}", s))
-        })
+        String::from_utf16_lossy(&self.s).fmt(f)
     }
 }
 
 impl fmt::Display for JSString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let s = pool.resolve(&self.s);
-            f.write_fmt(format_args!("{}", s))
-        })
+        String::from_utf16_lossy(&self.s).fmt(f)
+    }
+}
+
+impl Index<usize> for JSString {
+    type Output = u16;
+    fn index(&self, idx: usize) -> &Self::Output {
+        &self.as_slice()[idx]
     }
 }
 
 impl std::cmp::PartialEq<&str> for JSString {
     fn eq(&self, other: &&str) -> bool {
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let maybe_other_key = pool.get(*other);
-            if let Some(other_key) = maybe_other_key {
-                self.s == other_key
-            } else {
-                false
+        let mut ileft = self.s.iter();
+        let mut iright = (*other).encode_utf16();
+        loop {
+            let left = ileft.next();
+            let right = iright.next();
+            match (left, right) {
+                (None, None) => {
+                    break true;
+                }
+                (Some(_), None) | (None, Some(_)) => {
+                    break false;
+                }
+                (Some(&l), Some(r)) if l != r => {
+                    break false;
+                }
+                _ => {}
             }
-        })
+        }
     }
+}
+
+// Static Semantics: UTF16SurrogatePairToCodePoint ( lead, trail )
+//
+// The abstract operation UTF16SurrogatePairToCodePoint takes arguments lead (a code unit) and trail (a code unit). Two
+// code units that form a UTF-16 surrogate pair are converted to a code point. It performs the following steps when
+// called:
+//
+//  1. Assert: lead is a leading surrogate and trail is a trailing surrogate.
+//  2. Let cp be (lead - 0xD800) × 0x400 + (trail - 0xDC00) + 0x10000.
+//  3. Return the code point cp.
+fn utf16_surrogate_pair_to_code_point(lead: u16, trail: u16) -> u32 {
+    let cp: u32 = ((lead - 0xD800) as u32) * 0x400 + ((trail - 0xDC00) as u32) + 0x10000;
+    cp
+}
+
+// Static Semantics: CodePointAt ( string, position )
+//
+// The abstract operation CodePointAt takes arguments string (a String) and position (a non-negative integer). It
+// interprets string as a sequence of UTF-16 encoded code points, as described in 6.1.4, and reads from it a single code
+// point starting with the code unit at index position. It performs the following steps when called:
+//
+//  1. Let size be the length of string.
+//  2. Assert: position ≥ 0 and position < size.
+//  3. Let first be the code unit at index position within string.
+//  4. Let cp be the code point whose numeric value is that of first.
+//  5. If first is not a leading surrogate or trailing surrogate, then
+//      a. Return the Record { [[CodePoint]]: cp, [[CodeUnitCount]]: 1, [[IsUnpairedSurrogate]]: false }.
+//  6. If first is a trailing surrogate or position + 1 = size, then
+//      a. Return the Record { [[CodePoint]]: cp, [[CodeUnitCount]]: 1, [[IsUnpairedSurrogate]]: true }.
+//  7. Let second be the code unit at index position + 1 within string.
+//  8. If second is not a trailing surrogate, then
+//      a. Return the Record { [[CodePoint]]: cp, [[CodeUnitCount]]: 1, [[IsUnpairedSurrogate]]: true }.
+//  9. Set cp to ! UTF16SurrogatePairToCodePoint(first, second).
+//  10. Return the Record { [[CodePoint]]: cp, [[CodeUnitCount]]: 2, [[IsUnpairedSurrogate]]: false }.
+#[derive(PartialEq)]
+struct CodePointAtResult {
+    code_point: u32,
+    code_unit_count: u8,
+    is_unpaired_surrogate: bool,
+}
+fn code_point_at(string: &JSString, position: usize) -> CodePointAtResult {
+    let size = string.len();
+    let first = string[position];
+    let cp: u32 = first as u32;
+    if !(0xD800..=0xDFFF).contains(&first) {
+        CodePointAtResult { code_point: cp, code_unit_count: 1, is_unpaired_surrogate: false }
+    } else if first >= 0xDC00 || position + 1 == size {
+        CodePointAtResult { code_point: cp, code_unit_count: 1, is_unpaired_surrogate: true }
+    } else {
+        let second = string[position + 1];
+        if !(0xDC00..=0xDFFF).contains(&second) {
+            CodePointAtResult { code_point: cp, code_unit_count: 1, is_unpaired_surrogate: true }
+        } else {
+            let cp = utf16_surrogate_pair_to_code_point(first, second);
+            CodePointAtResult { code_point: cp, code_unit_count: 2, is_unpaired_surrogate: false }
+        }
+    }
+}
+
+// Static Semantics: StringToCodePoints ( string )
+//
+// The abstract operation StringToCodePoints takes argument string (a String). It returns the sequence of Unicode code
+// points that results from interpreting string as UTF-16 encoded Unicode text as described in 6.1.4. It performs the
+// following steps when called:
+//
+//  1. Let codePoints be a new empty List.
+//  2. Let size be the length of string.
+//  3. Let position be 0.
+//  4. Repeat, while position < size,
+//      a. Let cp be ! CodePointAt(string, position).
+//      b. Append cp.[[CodePoint]] to codePoints.
+//      c. Set position to position + cp.[[CodeUnitCount]].
+//  5. Return codePoints.
+fn string_to_code_points(string: &JSString) -> Vec<u32> {
+    // Note that this happily glosses over encoding errors. Storing in a Vec<u32> for now.
+    let size = string.len();
+    let mut code_points: Vec<u32> = Vec::with_capacity(size);
+    let mut position = 0;
+    while position < size {
+        let cp = code_point_at(string, position);
+        code_points.push(cp.code_point);
+        position += cp.code_unit_count as usize;
+    }
+    code_points
 }
 
 #[cfg(test)]
@@ -173,12 +187,6 @@ mod tests {
         let display = format!("{}", res);
         //assert_eq!(display, "Bob");
         assert!(display == "Bob");
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let words: Vec<u16> = res.words(&pool).collect();
-            //assert_eq!(words, src);
-            assert!(words == src);
-        })
     }
 
     #[test]
@@ -186,14 +194,13 @@ mod tests {
         let src: &[u16] = &[0x101, 0xDC67, 0xE00D, 0x1111, 0xE00E]; // not valid utf-16.
         let res = JSString::from(src);
         let display = format!("{}", res);
-        //assert_eq!(display, "\u{0101}\u{E00D}\u{0C67}\u{E00E}\u{000D}\u{1111}\u{E00E}\u{000E}");
-        assert!(display == "\u{0101}\u{E00D}\u{0C67}\u{E00E}\u{000D}\u{1111}\u{E00E}\u{000E}");
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let words: Vec<u16> = res.words(&pool).collect();
-            //assert_eq!(words, src);
-            assert!(words == src);
-        })
+        assert!(display == "\u{0101}\u{FFFD}\u{E00D}\u{1111}\u{E00E}");
+        assert!(res.len() == 5);
+        assert!(res[0] == 0x101);
+        assert!(res[1] == 0xdc67);
+        assert!(res[2] == 0xe00d);
+        assert!(res[3] == 0x1111);
+        assert!(res[4] == 0xe00e);
     }
 
     #[test]
@@ -203,12 +210,6 @@ mod tests {
         let display = format!("{}", res);
         //assert_eq!(display, "Bob");
         assert!(display == "Bob");
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let words: Vec<u16> = res.words(&pool).collect();
-            //assert_eq!(words, &[66, 111, 98]);
-            assert!(words == [66, 111, 98]);
-        })
     }
 
     #[test]
@@ -218,12 +219,6 @@ mod tests {
         let display = format!("{}", res);
         //assert_eq!(display, "Bob");
         assert!(display == "Bob");
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let words: Vec<u16> = res.words(&pool).collect();
-            //assert_eq!(words, &[66, 111, 98]);
-            assert!(words == [66, 111, 98]);
-        })
     }
     #[test]
     fn from_string_test_01() {
@@ -232,18 +227,11 @@ mod tests {
         let display = format!("{}", res);
         //assert_eq!(display, "Bob");
         assert!(display == "Bob");
-        STRING_POOL.with(|sp| {
-            let pool = sp.borrow();
-            let words: Vec<u16> = res.words(&pool).collect();
-            //assert_eq!(words, &[66, 111, 98]);
-            assert!(words == [66, 111, 98]);
-        })
     }
     #[test]
     fn debug_repr_test_01() {
         let jsstr = JSString::from("hello");
         let debug_str = format!("{:?}", jsstr);
-        //assert_eq!(debug_str, "\"hello\"");
         assert!(debug_str == "\"hello\"");
     }
     #[test]
@@ -251,29 +239,86 @@ mod tests {
         let s1 = JSString::from("blue");
         let s2 = JSString::from("orange");
         let s3 = JSString::from("blue");
+        let s4 = JSString::from("b");
         assert!(s1 == s3);
         assert!(s1 != s2);
         assert!(s2 != s3);
         assert!(s1 == "blue");
         assert!(s2 == "orange");
         assert!(s1 != "elephant");
-        //assert_ne!(s1, "orange");
         assert!(s1 != "orange");
+        assert!(s1 != s4);
+        assert!(s4 != s1);
+        assert!(s1 != "blueox");
+        assert!(s1 != "blu");
     }
 
-    #[test]
-    fn iterator_test_01() {
-        let mystr = "blue\u{E00E}";
-        let i = CharToU16Iterator::new(mystr);
-        let res: Vec<u16> = i.collect();
-        //assert_eq!(res, &[98, 108, 117, 101, 0xe00e]);
-        assert!(res == [98, 108, 117, 101, 0xe00e]);
-    }
 
     #[test]
     fn clone_test() {
         let s1 = JSString::from("crocodile");
         let s2 = s1.clone();
         assert!(s1 == s2);
+    }
+
+    #[test]
+    fn code_point_at_01() {
+        let mystr = JSString::from("test");
+        let r1 = code_point_at(&mystr, 0);
+        assert!(r1.code_point == 116);
+        assert!(r1.code_unit_count == 1);
+        assert!(!r1.is_unpaired_surrogate);
+        let r2 = code_point_at(&mystr, 1);
+        assert!(r2.code_point == 101);
+        assert!(r2.code_unit_count == 1);
+        assert!(!r2.is_unpaired_surrogate);
+        let r3 = code_point_at(&mystr, 2);
+        assert!(r3.code_point == 115);
+        assert!(r3.code_unit_count == 1);
+        assert!(!r3.is_unpaired_surrogate);
+        let r4 = code_point_at(&mystr, 0);
+        assert!(r4.code_point == 116);
+        assert!(r4.code_unit_count == 1);
+        assert!(!r4.is_unpaired_surrogate);
+    }
+
+    #[test]
+    fn code_point_at_02() {
+        let mystr = JSString::from(vec![0x00, 0xd800, 0xdc00, 0xde00, 0xd900, 0x00e2, 0xd902]);
+        let expected = vec![
+            CodePointAtResult { code_point: 0, code_unit_count: 1, is_unpaired_surrogate: false },
+            CodePointAtResult { code_point: 0x10000, code_unit_count: 2, is_unpaired_surrogate: false },
+            CodePointAtResult { code_point: 0xde00, code_unit_count: 1, is_unpaired_surrogate: true },
+            CodePointAtResult { code_point: 0xd900, code_unit_count: 1, is_unpaired_surrogate: true },
+            CodePointAtResult { code_point: 0xe2, code_unit_count: 1, is_unpaired_surrogate: false },
+            CodePointAtResult { code_point: 0xd902, code_unit_count: 1, is_unpaired_surrogate: true }
+        ];
+        let positions = vec![ 0, 1, 3, 4, 5, 6 ];
+        for idx in 0..positions.len() {
+            let result = code_point_at(&mystr, positions[idx]);
+            assert!(result == expected[idx]);
+        }
+    }
+
+    #[test]
+    fn code_point_at_03() {
+        // if any of the fields is different, they're not equal
+        let a = CodePointAtResult { code_point: 0x100, code_unit_count: 2, is_unpaired_surrogate: false };
+        let b1 = CodePointAtResult { code_point: 0x100, code_unit_count: 1, is_unpaired_surrogate: false };
+        let b2 = CodePointAtResult { code_point: 0x100, code_unit_count: 2, is_unpaired_surrogate: true };
+        let b3 = CodePointAtResult { code_point: 0x101, code_unit_count: 2, is_unpaired_surrogate: false };
+        assert!(a != b1);
+        assert!(a != b2);
+        assert!(a != b3);
+        let c = CodePointAtResult { code_point: 0x100, code_unit_count: 2, is_unpaired_surrogate: false };
+        assert!(a == c);
+    }
+
+    #[test]
+    fn string_to_code_points_01() {
+        let mystr = JSString::from(vec![0x00, 0xd800, 0xdc00, 0xde00, 0xd900, 0x00e2, 0xd902]);
+        let result = string_to_code_points(&mystr);
+        let expected = vec![0, 0x10000, 0xde00, 0xd900, 0xe2, 0xd902];
+        assert!(result == expected);
     }
 }
