7621c7b5ee3de7997514f05c56d9b933e73e7440
Steven Cole <scole66@gmail.com>
2021-06-21 07:23:47 -0700

    Move testhelp functions into their own file
    This helps with coverage statistics

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -473,39 +473,7 @@ pub mod update_expressions;
 pub mod with_statement;
 
 #[cfg(test)]
-pub mod testhelp {
-    use super::*;
-    use std::fmt;
-    pub fn check<T>(res: ParseResult<T>) -> (Rc<T>, Scanner) {
-        assert!(res.is_ok());
-        res.unwrap()
-    }
-    pub fn check_err<T>(res: ParseResult<T>, msg: &str, line: u32, column: u32)
-    where
-        T: fmt::Debug,
-    {
-        assert!(res.is_err());
-        let err = res.unwrap_err();
-        assert_eq!((err.msg, err.line, err.column), (String::from(msg), line, column));
-    }
-    pub fn chk_scan(scanner: &Scanner, count: u32) {
-        assert_eq!(*scanner, Scanner { line: 1, column: count + 1, start_idx: count as usize });
-    }
-    pub fn newparser(text: &str) -> Parser {
-        Parser::new(text, false, false, ParseGoal::Script)
-    }
-    pub fn check_parse_error<T, U>(result: ParseResult<T>, msg: U)
-    where
-        T: fmt::Debug,
-        U: Into<String>,
-    {
-        assert!(result.is_err());
-        let pe = result.unwrap_err();
-        assert_eq!(pe.line, 1);
-        assert_eq!(pe.column, 1);
-        assert_eq!(pe.msg, msg.into());
-    }
-}
+pub mod testhelp;
 
 #[cfg(test)]
 mod tests;
