bb9ec00eebb13ea0fab4bb8acdbf6f1a8e57aced
Steven Cole <scole66@gmail.com>
2021-07-13 11:07:46 -0700

    Add a TestObject that throws particular methods (#20)
    For helping out the test system. This is _nearly_ a mock object.

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,19 @@
 use super::agent::Agent;
-use super::cr::{AbruptCompletion, CompletionInfo};
-use super::object::{get, Object};
+use super::cr::{AbruptCompletion, AltCompletion, Completion, CompletionInfo};
+use super::errors::create_type_error;
+use super::object::{
+    get, ordinary_define_own_property, ordinary_delete, ordinary_get, ordinary_get_own_property, ordinary_get_prototype_of, ordinary_has_property, ordinary_is_extensible,
+    ordinary_own_property_keys, ordinary_prevent_extensions, ordinary_set, ordinary_set_prototype_of, CommonObjectData, Object, ObjectInterface, PotentialPropertyDescriptor,
+    PropertyDescriptor, ORDINARY_OBJECT_SLOTS,
+};
+use super::realm::IntrinsicId;
 use super::values::{ECMAScriptValue, PropertyKey};
 use ahash::RandomState;
+use std::cell::RefCell;
 use std::hash::{BuildHasher, Hash, Hasher};
 use std::io::Result as IoResult;
 use std::io::Write;
+use std::rc::Rc;
 
 pub struct MockWriter<T>
 where
@@ -112,4 +120,153 @@ pub fn test_agent() -> Agent {
     let mut agent = Agent::new();
     agent.initialize_host_defined_realm();
     agent
+}
+
+#[derive(Debug)]
+pub struct TestObject {
+    common: RefCell<CommonObjectData>,
+    get_prototype_of_throws: bool,
+    set_prototype_of_throws: bool,
+    is_extensible_throws: bool,
+    prevent_extensions_throws: bool,
+    get_own_property_throws: bool,
+    define_own_property_throws: bool,
+    has_property_throws: bool,
+    get_throws: bool,
+    set_throws: bool,
+    delete_throws: bool,
+    own_property_keys_throws: bool,
+}
+
+impl<'a> From<&'a TestObject> for &'a dyn ObjectInterface {
+    fn from(obj: &'a TestObject) -> Self {
+        obj
+    }
+}
+
+impl ObjectInterface for TestObject {
+    fn common_object_data(&self) -> &RefCell<CommonObjectData> {
+        &self.common
+    }
+    fn is_ordinary(&self) -> bool {
+        !(self.get_prototype_of_throws || self.set_prototype_of_throws)
+    }
+    fn id(&self) -> usize {
+        self.common.borrow().objid
+    }
+
+    fn get_prototype_of(&self, agent: &mut Agent) -> AltCompletion<Option<Object>> {
+        if self.get_prototype_of_throws {
+            Err(create_type_error(agent, "[[GetPrototypeOf]] called on TestObject"))
+        } else {
+            Ok(ordinary_get_prototype_of(self))
+        }
+    }
+    fn set_prototype_of(&self, agent: &mut Agent, obj: Option<Object>) -> AltCompletion<bool> {
+        if self.set_prototype_of_throws {
+            Err(create_type_error(agent, "[[SetPrototypeOf]] called on TestObject"))
+        } else {
+            Ok(ordinary_set_prototype_of(self, obj))
+        }
+    }
+    fn is_extensible(&self, agent: &mut Agent) -> AltCompletion<bool> {
+        if self.is_extensible_throws {
+            Err(create_type_error(agent, "[[IsExtensible]] called on TestObject"))
+        } else {
+            Ok(ordinary_is_extensible(self))
+        }
+    }
+    fn prevent_extensions(&self, agent: &mut Agent) -> AltCompletion<bool> {
+        if self.prevent_extensions_throws {
+            Err(create_type_error(agent, "[[PreventExtensions]] called on TestObject"))
+        } else {
+            Ok(ordinary_prevent_extensions(self))
+        }
+    }
+    fn get_own_property(&self, agent: &mut Agent, key: &PropertyKey) -> AltCompletion<Option<PropertyDescriptor>> {
+        if self.get_own_property_throws {
+            Err(create_type_error(agent, "[[GetOwnProperty]] called on TestObject"))
+        } else {
+            Ok(ordinary_get_own_property(self, key))
+        }
+    }
+    fn define_own_property(&self, agent: &mut Agent, key: &PropertyKey, desc: &PotentialPropertyDescriptor) -> AltCompletion<bool> {
+        if self.define_own_property_throws {
+            Err(create_type_error(agent, "[[DefineOwnProperty]] called on TestObject"))
+        } else {
+            ordinary_define_own_property(agent, self, key, desc)
+        }
+    }
+    fn has_property(&self, agent: &mut Agent, key: &PropertyKey) -> AltCompletion<bool> {
+        if self.has_property_throws {
+            Err(create_type_error(agent, "[[HasProperty]] called on TestObject"))
+        } else {
+            ordinary_has_property(agent, self, key)
+        }
+    }
+    fn get(&self, agent: &mut Agent, key: &PropertyKey, receiver: &ECMAScriptValue) -> Completion {
+        if self.get_throws {
+            Err(create_type_error(agent, "[[Get]] called on TestObject"))
+        } else {
+            ordinary_get(agent, self, key, receiver)
+        }
+    }
+    fn set(&self, agent: &mut Agent, key: &PropertyKey, value: &ECMAScriptValue, receiver: &ECMAScriptValue) -> AltCompletion<bool> {
+        if self.set_throws {
+            Err(create_type_error(agent, "[[Set]] called on TestObject"))
+        } else {
+            ordinary_set(agent, self, key, value, receiver)
+        }
+    }
+    fn delete(&self, agent: &mut Agent, key: &PropertyKey) -> AltCompletion<bool> {
+        if self.delete_throws {
+            Err(create_type_error(agent, "[[Delete]] called on TestObject"))
+        } else {
+            ordinary_delete(agent, self, key)
+        }
+    }
+    fn own_property_keys(&self, agent: &mut Agent) -> AltCompletion<Vec<PropertyKey>> {
+        if self.own_property_keys_throws {
+            Err(create_type_error(agent, "[[OwnPropertyKeys]] called on TestObject"))
+        } else {
+            Ok(ordinary_own_property_keys(self))
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum FunctionId {
+    GetPrototypeOf,
+    SetPrototypeOf,
+    IsExtensible,
+    PreventExtensions,
+    GetOwnProperty,
+    DefineOwnProperty,
+    HasProperty,
+    Get,
+    Set,
+    Delete,
+    OwnPropertyKeys,
+}
+
+impl TestObject {
+    pub fn object(agent: &mut Agent, throwers: &[FunctionId]) -> Object {
+        let prototype = agent.intrinsic(IntrinsicId::ObjectPrototype);
+        Object {
+            o: Rc::new(Self {
+                common: RefCell::new(CommonObjectData::new(agent, Some(prototype), true, &ORDINARY_OBJECT_SLOTS)),
+                get_prototype_of_throws: throwers.contains(&FunctionId::GetPrototypeOf),
+                set_prototype_of_throws: throwers.contains(&FunctionId::SetPrototypeOf),
+                is_extensible_throws: throwers.contains(&FunctionId::IsExtensible),
+                prevent_extensions_throws: throwers.contains(&FunctionId::PreventExtensions),
+                get_own_property_throws: throwers.contains(&FunctionId::GetOwnProperty),
+                define_own_property_throws: throwers.contains(&FunctionId::DefineOwnProperty),
+                has_property_throws: throwers.contains(&FunctionId::HasProperty),
+                get_throws: throwers.contains(&FunctionId::Get),
+                set_throws: throwers.contains(&FunctionId::Set),
+                delete_throws: throwers.contains(&FunctionId::Delete),
+                own_property_keys_throws: throwers.contains(&FunctionId::OwnPropertyKeys),
+            }),
+        }
+    }
 }
