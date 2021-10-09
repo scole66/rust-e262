6714f09048582c55c1ca3c7b3cfe495f378d6f63
Steven Cole <scole66@gmail.com>
2021-10-09 15:13:20 -0700

    Use the new funcs

--- src/parser/async_function_definitions/mod.rs
+++ src/parser/async_function_definitions/mod.rs
@@ -130,11 +130,11 @@ impl AsyncFunctionDeclaration {
         //  * It is a Syntax Error if FormalParameters Contains SuperCall is true.
         //  * It is a Syntax Error if AsyncFunctionBody Contains SuperCall is true.
         let mut errs = Vec::new();
-        //if self.body.function_body_contains_use_strict() && ! self.params.is_simple_parameter_list() {
-        //    // FunctionBodyContainsUseStrict of AsyncFunctionBody is true and IsSimpleParameterList of FormalParameters
-        //    // is false
-        //    errs.push(create_syntax_error_object(agent, "Strict functions must also have simple parameter lists"));
-        //}
+        if self.body.function_body_contains_use_strict() && ! self.params.is_simple_parameter_list() {
+            // FunctionBodyContainsUseStrict of AsyncFunctionBody is true and IsSimpleParameterList of FormalParameters
+            // is false
+            errs.push(create_syntax_error_object(agent, "Strict functions must also have simple parameter lists"));
+        }
         if self.params.contains(ParseNodeKind::AwaitExpression) {
             // FormalParameters Contains AwaitExpression is true.
             errs.push(create_syntax_error_object(agent, "Await not allowed in parameter lists"));
