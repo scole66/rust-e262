cbf7314c90592e27fe763639ed1733a2bdfa4d97
Steven Cole <scole66@gmail.com>
2022-04-29 07:42:56 -0700

    Still a wip, but rethinking completion records
    The idea being: don't let it be possible to represent an invalid
    combination.

--- src/cr/mod.rs
+++ src/cr/mod.rs
@@ -1,3 +1,4 @@
+use crate::reference::Reference;
 use crate::strings::JSString;
 use crate::values::ECMAScriptValue;
 
@@ -17,6 +18,36 @@ pub enum AbruptCompletion {
 
 pub type Completion = Result<ECMAScriptValue, AbruptCompletion>;
 pub type AltCompletion<T> = Result<T, AbruptCompletion>;
+
+pub enum NormalCompletion {
+    Empty,
+    Value(ECMAScriptValue),
+    Reference(Box<Reference>),
+}
+pub enum AbruptCompletion2 {
+    Break { value: Option<ECMAScriptValue>, target: Option<JSString> },
+    Continue { value: Option<ECMAScriptValue>, target: Option<JSString> },
+    Return { value: ECMAScriptValue },
+    Throw { value: ECMAScriptValue },
+}
+
+pub type Completion2 = Result<NormalCompletion, AbruptCompletion2>;
+
+pub fn update_empty(completion_record: Completion2, old_value: Option<ECMAScriptValue>) -> Completion2 {
+    match completion_record {
+        Ok(NormalCompletion::Empty) => match old_value {
+            None => Ok(NormalCompletion::Empty),
+            Some(v) => Ok(NormalCompletion::Value(v)),
+        },
+        Ok(_)
+        | Err(AbruptCompletion2::Return { .. })
+        | Err(AbruptCompletion2::Throw { .. })
+        | Err(AbruptCompletion2::Break { value: Some(_), .. })
+        | Err(AbruptCompletion2::Continue { value: Some(_), .. }) => completion_record,
+        Err(AbruptCompletion2::Break { value: None, target }) => Err(AbruptCompletion2::Break { value: old_value, target }),
+        Err(AbruptCompletion2::Continue { value: None, target }) => Err(AbruptCompletion2::Continue { value: old_value, target }),
+    }
+}
 
 #[cfg(test)]
 mod tests;
