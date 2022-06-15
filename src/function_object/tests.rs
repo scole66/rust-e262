34e52ce0d0cef9deba53896e96e2dc15a41fe414
Steven Cole <scole66@gmail.com>
2022-06-15 07:14:44 -0700

    Rename "Arguments" iterator to "FuncArgs" (#239)
    so that it doesn't name clash with the parse node structure of the same
    name.

--- src/function_object/tests.rs
+++ src/function_object/tests.rs
@@ -8,7 +8,7 @@ mod arguments {
     #[test]
     fn empty() {
         let arguments: &[ECMAScriptValue] = &[];
-        let mut args = Arguments::from(arguments);
+        let mut args = FuncArgs::from(arguments);
 
         assert_eq!(args.count(), 0);
         assert_eq!(args.next_arg(), ECMAScriptValue::Undefined);
@@ -17,7 +17,7 @@ mod arguments {
     #[test]
     fn list_of_two() {
         let arguments: &[ECMAScriptValue] = &[ECMAScriptValue::from(10), ECMAScriptValue::from(20)];
-        let mut args = Arguments::from(arguments);
+        let mut args = FuncArgs::from(arguments);
 
         assert_eq!(args.count(), 2);
         assert_eq!(args.next_arg(), ECMAScriptValue::from(10));
@@ -35,7 +35,7 @@ mod arguments {
             ECMAScriptValue::from(2),
             ECMAScriptValue::from(3),
         ];
-        let mut args = Arguments::from(arguments);
+        let mut args = FuncArgs::from(arguments);
 
         let first = args.next_arg();
         assert_eq!(first, ECMAScriptValue::from("first"));
