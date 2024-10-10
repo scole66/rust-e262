25531656caacdcaf3224efc4f0c8b6ee4fbbcfce
Steven Cole <scole66@gmail.com>
2024-10-10 10:01:33 -0700

    feat: integration tests for Array.prototype.map (#444)

--- src/tests/integration.rs
+++ src/tests/integration.rs
@@ -670,6 +670,31 @@ fn argument_list(src: &str) -> Result<ECMAScriptValue, String> {
     => serr("Thrown: 0");
     "Array.prototype.push: second Set fails"
 )]
+// Array.prototype.indexOf
+#[test_case("[1, 2, 3].indexOf(2)" => Ok(ECMAScriptValue::Number(1.0)); "Array.prototype.indexOf: item present")]
+#[test_case("[1, 2, 3].indexOf(99)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: item not present")]
+#[test_case("[].indexOf('bob')" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: empty list")]
+#[test_case("[1, 2, 3, 2, 1].indexOf(2, -3)" => Ok(ECMAScriptValue::Number(3.0)); "Array.prototype.indexOf: from right")]
+#[test_case("[1, 2, 3].indexOf(1, Infinity)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: infinite start index")]
+#[test_case("Array.prototype.indexOf.call(undefined, [], 2)" => serr("Thrown: TypeError: Undefined and null cannot be converted to objects"); "Array.prototype.indexOf: Non-object this")]
+#[test_case("Array.prototype.indexOf.call({length: Symbol.toStringTag})" => serr("Thrown: TypeError: Symbol values cannot be converted to Number values"); "Array.prototype.indexOf: not array-like")]
+#[test_case("[1, 2].indexOf(1, Symbol.toStringTag)" => serr("Thrown: TypeError: Symbol values cannot be converted to Number values"); "Array.prototype.indexOf: bad starting index")]
+#[test_case("[1, 2, 3, 1].indexOf(1, -Infinity)" => Ok(ECMAScriptValue::Number(0.0)); "Array.prototype.indexOf: starting index -inf")]
+#[test_case("Array.prototype.indexOf.call({length: 2, get [1]() { throw 'oops' }}, 7)" => serr("Thrown: oops"); "Array.prototype.indexOf: get throws")]
+#[test_case("new Proxy([1],{has(t,p){throw'oops';}}).indexOf(1)" => serr("Thrown: oops"); "Array.prototype.indexOf: HasProperty throws")]
+#[test_case("[1, 3, 9].indexOf(3, 10)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: starting index beyond length")]
+#[test_case("[1, 3, 9].indexOf(3, -10000)" => Ok(ECMAScriptValue::Number(1.0)); "Array.prototype.indexOf: starting index before start")]
+// Array.prototype.map
+#[test_case("[1, 2, 3].map(x => x + 10).join(', ')" => Ok(ECMAScriptValue::from("11, 12, 13")); "Array.prototype.map: items present")]
+#[test_case("Array.prototype.map.call(undefined)" => serr("Thrown: TypeError: Undefined and null cannot be converted to objects"); "Array.prototype.map: bad this")]
+#[test_case("Array.prototype.map.call({length: Symbol.toStringTag})" => serr("Thrown: TypeError: Symbol values cannot be converted to Number values"); "Array.prototype.map: not arraylike")]
+#[test_case("[].map(9)" => serr("Thrown: TypeError: Array.prototype.map: callback function was not callable"); "Array.prototype.map: non-callable callback")]
+#[test_case("Array.prototype.map.call({length: 999999999999999999999999999999}, x => x + 1)" => serr("Thrown: RangeError: Array lengths greater than 4294967295 are not allowed"); "Array.prototype.map: array_species_create fails")]
+#[test_case("new Proxy([1],{has(t,p){throw'oops';}}).map(x=>x+1)" => serr("Thrown: oops"); "Array.prototype.map: [[HasProperty]] fails")]
+#[test_case("Array.prototype.map.call({length:3,get[1](){throw'oops';}},x=>x+1)" => serr("Thrown: oops"); "Array.prototype.map: [[Get]] fails")]
+#[test_case("[1].map(x=>{throw'oops';})" => serr("Thrown: oops"); "Array.prototype.map: callback throws")]
+#[test_case("class M extends Array{constructor(){super();Object.defineProperty(this,'2',{configurable:false})}};(new M()).map(x=>x)" => serr("Thrown: TypeError: Unable to create data property"); "Array.prototype.map: CreateDataPropertyOrThrow fails")]
+#[test_case("class M extends Array{constructor(name,...args){super(...args);this.token=name;}}; (new M('sentinel', 10, 652, 3)).map((val,idx,obj)=>`${obj.token}~${idx}~${val}`).join(',')" => vok("sentinel~0~10,sentinel~1~652,sentinel~2~3"); "Array.prototype.map: all the function args are correct")]
 // MethodDefinition: get ClassElementName ( ) { FunctionBody }
 #[test_case("let o = { get a() { return 3; } }; o.a" => vok(3); "method_def_get_call_works")]
 #[test_case("let o = { get a() { return 3; } }; Object.getOwnPropertyDescriptor(o, 'a').get.name" => vok("get a"); "method_def_get_name")]
@@ -828,20 +853,6 @@ fn argument_list(src: &str) -> Result<ECMAScriptValue, String> {
     => serr("Thrown: TypeError: @@~ token detected. aborting compilation.");
     "class: method compile fails"
 )]
-// Array.prototype.indexOf
-#[test_case("[1, 2, 3].indexOf(2)" => Ok(ECMAScriptValue::Number(1.0)); "Array.prototype.indexOf: item present")]
-#[test_case("[1, 2, 3].indexOf(99)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: item not present")]
-#[test_case("[].indexOf('bob')" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: empty list")]
-#[test_case("[1, 2, 3, 2, 1].indexOf(2, -3)" => Ok(ECMAScriptValue::Number(3.0)); "Array.prototype.indexOf: from right")]
-#[test_case("[1, 2, 3].indexOf(1, Infinity)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: infinite start index")]
-#[test_case("Array.prototype.indexOf.call(undefined, [], 2)" => serr("Thrown: TypeError: Undefined and null cannot be converted to objects"); "Array.prototype.indexOf: Non-object this")]
-#[test_case("Array.prototype.indexOf.call({length: Symbol.toStringTag})" => serr("Thrown: TypeError: Symbol values cannot be converted to Number values"); "Array.prototype.indexOf: not array-like")]
-#[test_case("[1, 2].indexOf(1, Symbol.toStringTag)" => serr("Thrown: TypeError: Symbol values cannot be converted to Number values"); "Array.prototype.indexOf: bad starting index")]
-#[test_case("[1, 2, 3, 1].indexOf(1, -Infinity)" => Ok(ECMAScriptValue::Number(0.0)); "Array.prototype.indexOf: starting index -inf")]
-#[test_case("Array.prototype.indexOf.call({length: 2, get [1]() { throw 'oops' }}, 7)" => serr("Thrown: oops"); "Array.prototype.indexOf: get throws")]
-#[test_case("new Proxy([1],{has(t,p){throw'oops';}}).indexOf(1)" => serr("Thrown: oops"); "Array.prototype.indexOf: HasProperty throws")]
-#[test_case("[1, 3, 9].indexOf(3, 10)" => Ok(ECMAScriptValue::Number(-1.0)); "Array.prototype.indexOf: starting index beyond length")]
-#[test_case("[1, 3, 9].indexOf(3, -10000)" => Ok(ECMAScriptValue::Number(1.0)); "Array.prototype.indexOf: starting index before start")]
 // ############# Random "it didn't work right" source text #############
 // This first item is 4/23/2023: the stack is messed up for errors in function parameters
 #[test_case("function id(x=(()=>{throw 'howdy';})()) {
